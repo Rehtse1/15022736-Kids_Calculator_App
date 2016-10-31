46843af9e77b80bf759e4554cad4ea481cbc4d88
rehtsek <esthery19@gmail.com>
2016-10-31 19:39:18 +0000

    Randomised numbers

--- Esther_CalculatorApp/Esther_CalculatorApp/ViewController.swift
+++ Esther_CalculatorApp/Esther_CalculatorApp/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var numberView1: UILabel!
+    @IBOutlet weak var numberView2: UILabel!
+    @IBOutlet weak var answerView: UILabel!
+    
+    @IBOutlet weak var resultsView: UIView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        let number1 = Int(arc4random_uniform(10))   
+        let number2 = Int(arc4random_uniform(10))
+        
+        numberView1.text = String(number1)
+        numberView2.text = String(number2)
+        
+        self.resultsView.isHidden = false
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
